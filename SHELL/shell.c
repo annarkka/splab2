a353b4eb162b1feabdbbbc80c5d7e25578699562
annarkka <38548842+annarkka@users.noreply.github.com>
2019-11-12 00:07:34 +0600

    Update shell.c

--- SHELL/shell.c
+++ SHELL/shell.c
@@ -1,1 +1,40 @@
 // your code here
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <wait.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+
+	char input[1000];
+	char* arr[100];
+
+	arr[0] = "/bin/bash";
+	arr[1] = "-c";
+	arr[3] = NULL;
+	while(1){
+		printf("$ Enter your command: ");
+		fgets(input, 1000, stdin);
+		char *pch = strtok(input, "\n");
+		arr[2] = input;
+
+		if(strcmp(pch, "Exit ") == 0){
+		break;
+		}
+
+		pid_t qwe = fork();
+		if (qwe == 0){
+			execvp(arr[0], arr);
+		}
+
+		waitpid(qwe, NULL,0);
+	}
+	return 0;
+
+}
